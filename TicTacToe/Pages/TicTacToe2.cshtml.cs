2664b8418970b621f682d0a6ec7efa4b2e1dcf28
Zane Roberts <zmroberts17@ksu.edu>
2019-11-12 15:29:30 -0600

    This commit completes the functionality of TicTacToe

---
+++ TicTacToe/Pages/TicTacToe2.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TicTacToe.Pages
+{
+    public class TicTacToe2Model : PageModel
+    {
+        public char[,] Board { get; } = new char[3, 3];
+        public void OnGet()
+        {
+            Board[0, 0] = 'X';
+        }
+    }
+}
